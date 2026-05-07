6eadbb442801b515e38a7b635b81293630801d7e
Ashwin0410 <programmingashwin@gmail.com>
2026-05-07 17:13:23 +0100

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import cfg
-
+# DB Starts
 engine = create_engine(
     cfg.DB_URL,
     connect_args={"check_same_thread": False} if "sqlite" in cfg.DB_URL else {},
